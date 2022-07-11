61e361bb74f4a50aee808ff4fe0c9451f8d06431
heerucan <iwpow0307@gmail.com>
2022-07-11 09:15:47 +0900

    [#2] launch screen sleep 적용

--- NewlyCoinedWord/NewlyCoinedWord/AppDelegate.swift
+++ NewlyCoinedWord/NewlyCoinedWord/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        sleep(3)
+        sleep(3)
         return true
     }
 
