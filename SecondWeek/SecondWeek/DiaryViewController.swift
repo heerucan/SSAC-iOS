7172d5f26c0a012b2e091195a1d796fd3a109e8f
heerucan <iwpow0307@gmail.com>
2022-07-11 17:29:20 +0900

    [#3] 감정다이어리 과제 구현

--- SecondWeek/SecondWeek/DiaryViewController.swift
+++ SecondWeek/SecondWeek/DiaryViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class DiaryViewController: UIViewController {
+    
+    var countList = [0, 0, 0,
+                 0, 0, 0,
+                 0, 0, 0]
+    
+    var emotionList = ["행복해", "사랑해", "좋아해",
+                       "당황해", "속상해", "우울해",
+                       "심심해", "찝찝해", "억울해"]
 
+    @IBOutlet var labelList: [UILabel]!
+    @IBOutlet var buttonList: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func clickedEmotionButton(_ sender: UIButton) {
+        for i in 0...buttonList.count-1 {
+            switch sender {
+            case buttonList[i]:
+                countList[i] += 1
+                labelList[i].text = emotionList[i] + " \(countList[i])"
+            default: break
+            }
+        }
     }
-    */
-
 }
