2308386425d4fedcccac50eef5adcb09e64d84d9
heerucan <iwpow0307@gmail.com>
2022-07-13 16:04:45 +0900

    [#3] DatePicker Project UI 구현

--- 2week/SecondWeek/SecondWeek/ViewController/DatePickerViewController.swift
+++ 2week/SecondWeek/SecondWeek/ViewController/DatePickerViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet var dayImageView: [UIImageView]!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var oneLabel: UILabel!
+    @IBOutlet weak var twoLabel: UILabel!
+    @IBOutlet weak var threeLabel: UILabel!
+    @IBOutlet weak var fourLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureDatePicker()
+        configureImageView()
+    }
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        print(sender.date)
+    }
+    
+    func configureDatePicker() {
+        datePicker.preferredDatePickerStyle = .wheels
+    }
+    
+    func configureImageView() {
+        dayImageView.forEach { view in
+            view.layer.cornerRadius = 10
+            view.contentMode = .scaleAspectFill
+            
+        }
     }
 
 }
