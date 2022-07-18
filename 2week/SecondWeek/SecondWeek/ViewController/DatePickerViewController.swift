d95a2d75db08e3657d0d7ed390cf9f6cb57aff77
heerucan <iwpow0307@gmail.com>
2022-07-18 09:57:12 +0900

    [#3] 초기값 있게끔 함수화 / 에셋변경

--- 2week/SecondWeek/SecondWeek/ViewController/DatePickerViewController.swift
+++ 2week/SecondWeek/SecondWeek/ViewController/DatePickerViewController.swift
@@ -11,7 +11,6 @@ class DatePickerViewController: UIViewController {
     
     @IBOutlet var dayImageView: [UIImageView]!
     @IBOutlet weak var datePicker: UIDatePicker!
-    
     @IBOutlet var dateLabelCollection: [UILabel]!
     
     override func viewDidLoad() {
@@ -21,29 +20,10 @@ class DatePickerViewController: UIViewController {
     }
     
     @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
-        let selectedDate = sender.date
-        
-        for i in 0...3 {
-            setupDateLabel(date: selectedDate.addingTimeInterval(TimeInterval((24*60*60*100*(i+1)))), index: i)
-        }
+        configureDatePicker(sender.date)
     }
     
-    func configureDatePicker() {
-        if #available(iOS 14.0, *) {
-            datePicker.preferredDatePickerStyle = .inline
-        } else {
-            datePicker.preferredDatePickerStyle = .wheels
-        }
-    }
-    
-    func configureImageView() {
-        dayImageView.forEach { view in
-            view.layer.cornerRadius = 10
-            view.contentMode = .scaleAspectFill
-        }
-    }
-    
-    func setupDateLabel(date: Date, index: Int) {
+    func setupDateLabel(date: Date = .now, index: Int) {
         dateLabelCollection.forEach { label in
             label.textAlignment = .center
             label.numberOfLines = 2
@@ -56,5 +36,25 @@ class DatePickerViewController: UIViewController {
         format.locale = Locale(identifier: "ko_KR")
         
         dateLabelCollection[index].text = format.string(from: date)
+    }
+    
+    func configureDatePicker(_ date: Date = .now) {
+        for i in 0...3 {
+            setupDateLabel(date: date.addingTimeInterval(TimeInterval((24*60*60*100*(i+1)))),
+                           index: i)
+        }
+        
+        if #available(iOS 14.0, *) {
+            datePicker.preferredDatePickerStyle = .inline
+        } else {
+            datePicker.preferredDatePickerStyle = .wheels
+        }
+    }
+    
+    func configureImageView() {
+        dayImageView.forEach { view in
+            view.layer.cornerRadius = 10
+            view.contentMode = .scaleAspectFill
+        }
     }
 }
