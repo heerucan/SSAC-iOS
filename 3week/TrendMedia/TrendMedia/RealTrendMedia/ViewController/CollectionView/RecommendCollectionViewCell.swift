af3f0b03e9d95aba79a017ee914064d065104e57
heerucan <iwpow0307@gmail.com>
2022-07-20 12:50:35 +0900

    [#7] 7/20 수업 내용 추가

---
+++ 3week/TrendMedia/TrendMedia/RealTrendMedia/ViewController/CollectionView/RecommendCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecommendCollectionViewCell.swift
+//  TrendMedia
+//
+//  Created by heerucan on 2022/07/20.
+//
+
+import UIKit
+
+class RecommendCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
---
+++ 3week/TrendMedia/TrendMedia/RealTrendMedia/ViewController/CollectionView/RecommendCollectionViewController.swift
@@ -0,0 +1,63 @@
+//
+//  RecommendCollectionViewController.swift
+//  TrendMedia
+//
+//  Created by heerucan on 2022/07/20.
+//
+
+import UIKit
+
+import Kingfisher
+import Toast
+
+/*
+ TableView -> CollectionView
+ Row -> Item
+ heightForRow -> FlowLayout (heightForRow)
+ */
+
+class RecommendCollectionViewController: UICollectionViewController {
+    
+    var imageURL = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA2MjBfMjg1%2FMDAxNjU1NzI3MTk1ODA3.ED4Enwxy9cLt_KWG9VQZDmYMfcv6sxOZfChWkFns0W4g.aZm2h8SUIGvLvUG3gO8nnX8FC5tyXswS1UXABDfg-kgg.JPEG.yunhsy%2Fsearch.pstatic.jpg&type=a340"
+
+    @IBOutlet var recommendCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 컬렉션뷰의 셀 크기, 셀 사이의 간격 등 설정
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let width = UIScreen.main.bounds.width - (spacing * 4)
+
+        // 셀이 가지고 있는 크기
+        layout.itemSize = CGSize(width: width/3, height: width/3)
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        
+        // horizontal일 때는 좌우사이의 간격 / vertical일 때는 상하사이의 간격
+        layout.minimumLineSpacing = spacing
+        // horizontal일 때는 상하사이의 간격
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommendCell", for: indexPath) as! RecommendCollectionViewCell
+        cell.posterImageView.backgroundColor = .brown
+        let url = URL(string: imageURL)
+        cell.posterImageView.kf.setImage(with: url)
+        return cell
+    }
+    
+    // cell 선택
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // 왜 view일까? 그건 그냥 uiview가 깔려 있는 게 아니라 애플이 이름을 view로 명칭해뒀으니까..
+        self.view.makeToast("\(indexPath.item)번 째 셀을 선택했당!", duration: 1.0, position: .center)
+    }
+    
+}
