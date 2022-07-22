79a1f8a1d6336bacbce389feb7ff75690f384858
heerucan <iwpow0307@gmail.com>
2022-07-22 12:45:41 +0900

    [#7] 7/22 swift 문법 - lazy var - 타입 프로퍼티 - struct / class - 인스턴스

--- 3week/ThirdPlayground.playground/Pages/First.xcplaygroundpage/Contents.swift
+++ 3week/ThirdPlayground.playground/Pages/First.xcplaygroundpage/Contents.swift
@@ -9,9 +9,9 @@ var shoplist: String? = "신발 사기"
 var shoplist2: String? = "모니터 사기"
 
 if shoplist != nil && shoplist2 != nil {
-    print("\(shoplist!) \(shoplist2!) 완료")
+//    print("\(shoplist!) \(shoplist2!) 완료")
 } else {
-    print("글자를 입력해주세요")
+//    print("글자를 입력해주세요")
 }
 
 // 2글자 이상을 꼭 입력해야 한다면?
@@ -22,7 +22,7 @@ if let jack = shoplist,
    let value = shoplist2, (2...6).contains(jack.count) == true {
     print("\(jack) \(value) 완료")
 } else {
-    print("글자를 입력해주세요")
+//    print("글자를 입력해주세요")
 }
 
 func optionalBindingFunction() {
@@ -30,11 +30,11 @@ func optionalBindingFunction() {
     guard let jack = shoplist,
           let value = shoplist2,
           (2...6).contains(jack.count) else {
-              print("글자를 입력해주세요")
+//              print("글자를 입력해주세요")
               return
           }
     
-    print("\(jack), \(value) 완료")
+//    print("\(jack), \(value) 완료")
 }
 
 // 2. 인스턴스 프로퍼티 vs 타입 프로퍼티
@@ -51,7 +51,7 @@ class User { // 값을 가지고 있는 상태에서 만들어줘야 함
 
 let user = User() // 클래스 초기화, 인스턴스 생성
 user.nickname = "칙촉" //
-print(user.nickname)
+//print(user.nickname)
 
  
 // static member 'red'는 cannot be used instance of type 'UIColor'
@@ -66,7 +66,7 @@ let user3 = User()
 User.staticNickname // 호출 시 메모리에 올라감, 인스턴스를 생성한다고 해서 초기화되지 않고, 호출해서 사용할 때 초기화가 된다. 그리고 한 번 올라가면 앱이 꺼질 떄까지 메모리에서 사라지지 않는다.
 
 User.staticNickname = "칙촉"
-print(User.staticNickname)
+//print(User.staticNickname)
 
 // 역할 기준 - 저장, 연산, 감시자
 // 메모리 기준 - 인스턴스, 타입
---
+++ 3week/ThirdPlayground.playground/Pages/Second.xcplaygroundpage/Contents.swift
@@ -0,0 +1,89 @@
+//: [Previous](@previous)
+
+import UIKit
+
+enum DrinkSize {
+    case short, tall, grande, venti
+}
+
+class DrinkClass {
+    let name: String
+    var count: Int
+    var size: DrinkSize
+    
+    // 클래스는 초기화 구문이 필요하다. > 상속에 이유가 있지 않을까?
+    // 부모 클래스도 초기화가 되어 있어야 상속을 받을 수 있으니까
+    // 반면에 struct는 상속이 안되니까..
+    init(name: String, count: Int, size: DrinkSize) {
+        self.name = name
+        self.count = count
+        self.size = size
+    }
+}
+
+struct DrinkStruct {
+    let name: String
+    var count: Int
+    var size: DrinkSize
+}
+
+let drinkClass = DrinkClass(name: "스무디", count: 1, size: .grande)
+drinkClass.count = 5
+drinkClass.size = .tall
+
+print(drinkClass.name, drinkClass.count, drinkClass.size)
+
+// name은 상수로 선언해서 변경을 못하지만 나머지는 값을 변경 가능하다.
+// 근데 drinkClass는 let인데 왜 값을 변경해줄 수 있을까?
+// ㄴ 왜냐하면 클래스는 주소 공간만 서로 공유하고 실제값이 있는 곳은 서로 다르기 때문(참조타입)
+
+
+let drinkStruct = DrinkStruct(name: "스무디", count: 1, size: .grande)
+//drinkStruct.count = 10
+//drinkStruct.size = .grande
+
+print(drinkStruct.name, drinkClass.count, drinkClass.size)
+
+// 근데 struct는 let인 경우에 오류가 발생한다. 왜일까?
+// ㄴ 왜냐하면 구조체는 값이 저장되는 공간 자체를 서로 공유하기 때문에 하나가 바뀌면 아예 다 바뀜(값타입)
+
+
+
+// 영화 타이틀, 러닝타임, 영상/화질좋은포스터 => 필요한 시점에 초기화를 할 수 없을까?
+
+
+struct Poster {
+    var image: UIImage = UIImage(named: "star") ?? UIImage()
+    
+    init() { // 작성하지 않아도 되는데 초기화되는지 궁금해서 쓴 것임
+        print("Poster initialized")
+    }
+}
+
+struct MediaInfo {
+    var mediaTitle: String
+    var mediaRuntime: Int
+    lazy var mediaPoster: Poster = Poster()
+}
+
+var media = MediaInfo(mediaTitle: "오징어게임", mediaRuntime: 123)
+
+// lazy var는 언제 동작되나? -> 필요한 시점에 사용을 할 때 메모리에 올라감
+// 저장 프로퍼티인데 저장을 지연시키는 것 = 저장 지연 프로퍼티
+// 언제 쓰지? -> 옵션적인 상황이 있거나, 초반에 초기화하기 부담스러운 용량이나 시간일 때,
+
+// lazy let은 불가능함
+// ㄴ let은 인스턴스가 생성되기 전에 값을 가지고 있어야 함 -> 메모리에 올라갈 때
+// ㄴ 근데 lazy는 값을 갖고 있음 안돼! 실제로 호출되어 사용되기 전까지는 nil인 상태로 유지되니까!
+media.mediaPoster
+
+
+// 타입 프로퍼티
+struct User {
+    static let name = "고래밥"
+    static let age = 33 // 저장 타입 프로퍼티
+}
+
+User.name // 호출하는 시점에 메모리에 올라간다. -> 호출을 하지 않으면 메모리에 올라가지 않음
+// ㄴ. 이건 lazy 지연 저장 프로퍼티와 동작 방식이 항상 같아서 lazy 키워드를 굳이 쓰지 않아도 됨
+
