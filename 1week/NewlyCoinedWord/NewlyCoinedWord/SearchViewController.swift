f4fca6b63161d340964e708e330eae633dd4c4e9
heerucan <iwpow0307@gmail.com>
2022-07-27 15:15:37 +0900

    [#8] 7/27 UIViewController에 테이블뷰 프로토콜 채택해서 사용하는 방법 - XIB 방식

--- 1week/NewlyCoinedWord/NewlyCoinedWord/SearchViewController.swift
+++ 1week/NewlyCoinedWord/NewlyCoinedWord/SearchViewController.swift
@@ -7,19 +7,34 @@
 
 import UIKit
 
+enum Word: String, CaseIterable {
+    case 캘박, 좋댓구알, 구취, 꾸안꾸, 실매
+    
+//    var word: String {
+//        switch self {
+//        case .캘박: return "캘박"
+//        case .좋댓구알: return "좋댓구알"
+//        case .구취: return "구취"
+//        case .꾸안꾸: return "꾸안꾸"
+//        case .실매: return "실매"
+//        }
+//    }
+    
+    var mean: String {
+        switch self {
+        case .캘박: return "캘린더에 일정을 픽스한다는 뜻이다"
+        case .좋댓구알: return "좋아요, 댓글, 구독, 알림설정을 의미한다"
+        case .구취: return "구독 취소를 의미한다"
+        case .꾸안꾸: return "꾸민듯 안꾸민듯이라는 뜻이다"
+        case .실매: return "실시간 매니저를 의미한다"
+        }
+    }
+}
+
 class SearchViewController: UIViewController {
     
-    // MARK: - Property
-    
-    var words = ["캘박", "좋댓구알", "구취", "꾸안꾸", "실매"]
-    var wordsDictionary = ["캘박": "캘린더에 일정을 픽스한다는 뜻이다",
-                           "좋댓구알좋댓구알": "좋아요, 댓글, 구독, 알림설정을 의미한다",
-                           "구취": "구독취소를 의미한다",
-                           "꾸안꾸": "꾸민듯 안꾸민듯이라는 뜻이다",
-                           "실매": "실시간 매니저를 의미한다"]
-
     // MARK: - @IBOutlet
-    
+        
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet var tagButtonList: [UIButton]!
     @IBOutlet weak var resultLabel: UILabel!
@@ -29,6 +44,9 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setTextField()
+//        Word.allCases.forEach {
+//            wordArray.append($0.rawValue)
+//        }
     }
     
     // MARK: - @IBAction
@@ -37,7 +55,13 @@ class SearchViewController: UIViewController {
         view.endEditing(true)
     }
     
+    @IBAction func tagButtonClicked(_ sender: UIButton) {
+        searchTextField.text = sender.currentTitle
+        clickedSearchButton(sender)
+    }
+    
     @IBAction func clickedSearchButton(_ sender: UIButton) {
+        print("눌러짐")
         setButton()
         searchWords()
         view.endEditing(true)
@@ -46,14 +70,16 @@ class SearchViewController: UIViewController {
     // MARK: - UDF
     
     func setButton() {
-        for i in (0...tagButtonList.count-1) {
-            tagButtonList[i].titleLabel?.numberOfLines = 1
-            tagButtonList[i].setTitle(words[i], for: .normal)
-            tagButtonList[i].setTitleColor(.black, for: .normal)
-            tagButtonList[i].layer.cornerRadius = 10
-            tagButtonList[i].layer.borderColor = UIColor.black.cgColor
-            tagButtonList[i].layer.borderWidth = 1
+        tagButtonList.forEach {
+            $0.titleLabel?.numberOfLines = 1
+            $0.setTitleColor(.black, for: .normal)
+            $0.layer.cornerRadius = 10
+            $0.layer.borderColor = UIColor.black.cgColor
+            $0.layer.borderWidth = 1
+            $0.setTitle(Word.allCases.map { $0.rawValue }.randomElement(), for: .normal)
         }
+        
+        resultLabel.text = Word.allCases.map { $0.mean }.randomElement()
     }
 
     func setTextField() {
@@ -68,22 +94,26 @@ class SearchViewController: UIViewController {
         searchTextField.rightViewMode = .always
     }
     
+    
     func searchWords() {
         guard let searchText = searchTextField.text else { return }
         
-        if words.contains(searchText) {
-            resultLabel.text = wordsDictionary[searchText]
-            
-            if let index = words.firstIndex(where: { $0 == searchText} ) {
-                words.remove(at: index)
-                words.shuffle()
-                words.insert(searchText, at: 0)
-                setButton()
-            }
-            
-        } else {
-            resultLabel.text = "없는 신조어이거나 뜻을 찾을 수 없습니다."
-        }
+        
+//        resultLabel.text = Word.mean.
+        
+//        if Dictionary.word.array.contains(searchText) {
+//            resultLabel.text = Dictionary.meaning.array[searchText]
+//
+//            if let index = Dictionary.word.array.firstIndex(where: { $0 == searchText} ) {
+//                Dictionary.word.array.remove(at: index)
+//                Dictionary.word.array.shuffle()
+//                Dictionary.word.array.insert(searchText, at: 0)
+//                setButton()
+//            }
+//
+//        } else {
+//            resultLabel.text = "없는 신조어이거나 뜻을 찾을 수 없습니다."
+//        }
     }
 }
 
---
+++ 4week/NetworkBasic/NetworkBasic/Screen/SearchViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SearchViewController.swift
+//  NetworkBasic
+//
+//  Created by heerucan on 2022/07/27.
+//
+
+import UIKit
+
+/*
+ Swift Protocol
+ - Delegate
+ - DataSource
+ */
+
+final class SearchViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    @IBOutlet weak var searchTableView: UITableView!
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        configureTableView()
+    }
+    
+    // MARK: - InitUI
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func configureTableView() {
+        // 연결고리 작업 : 테이블뷰가 해야 할 역할 > 뷰 컨트롤러에게 요청
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        
+        // 테이블뷰가 사용할 테이블뷰 셀(XIB) 등록 작업
+        // XIB : xml Interface Builder <= NIB
+        searchTableView.register(UINib(nibName: ListTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: ListTableViewCell.identifier)
+    }
+    
+    // MARK: - Custom Method
+
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as? ListTableViewCell else { return UITableViewCell() }
+        
+        cell.titleLabel.font = .boldSystemFont(ofSize: 22)
+        cell.titleLabel.text = "HELLOOOOOO"
+        return cell
+    }
+}
