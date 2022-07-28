5b32d533449f0cfdaf0fe5aa4d32b6947e70bb94
heerucan <iwpow0307@gmail.com>
2022-07-28 19:32:20 +0900

    [#8] 피커뷰 내리기 / 복붙 기능 막기 - 커스텀 클래스 사용해서 canPerformAction - UITextFieldDelegate 사용/툴바 사용

--- 4week/NetworkBasic/NetworkBasic/Screen/Lotto/LottoViewController.swift
+++ 4week/NetworkBasic/NetworkBasic/Screen/Lotto/LottoViewController.swift
@@ -12,12 +12,14 @@ final class LottoViewController: UIViewController {
     // MARK: - Property
     
     let numberList: [Int] = Array(1...1025).reversed()
-
+    
     // MARK: - IBOutlet
     
     @IBOutlet weak var numberTextField: UITextField!
     
     var lottoPickerView = UIPickerView()
+    var toolBar = UIToolbar()
+    let doneButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(touchupDoneButton))
     
     // MARK: - LifeCycle
     
@@ -25,8 +27,9 @@ final class LottoViewController: UIViewController {
         super.viewDidLoad()
         configurePickerView()
         configureTextField()
+        configureToolbar()
     }
-
+    
     // MARK: - Custom Method
     
     private func configurePickerView() {
@@ -36,9 +39,28 @@ final class LottoViewController: UIViewController {
     
     private func configureTextField() {
         numberTextField.inputView = lottoPickerView
-        
+        numberTextField.delegate = self
+        numberTextField.resignFirstResponder()
     }
     
+    func configureToolbar() {
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.sizeToFit()
+        
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        numberTextField.inputAccessoryView = toolBar
+    }
+    
+    // MARK: - @objc
+    
+    @objc func touchupDoneButton() {
+        let row = self.lottoPickerView.selectedRow(inComponent: 0)
+        self.lottoPickerView.selectRow(row, inComponent: 0, animated: false)
+        self.numberTextField.text = "\(self.numberList[row])회차 로또"
+        self.numberTextField.resignFirstResponder()
+    }
 }
 
 // MARK: - UIPickerViewDelegate, UIPickerViewDataSource
@@ -60,5 +82,30 @@ extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         // 각각의 열에 어떤 글자를 넣어줄 건데?
         return "\(numberList[row])회차"
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension LottoViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        numberTextField.isUserInteractionEnabled = false
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        numberTextField.isUserInteractionEnabled = true
+    }
+}
+
+// MARK: - 커스텀클래스
+
+class CustomTextField: UITextField {
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(UIResponderStandardEditActions.paste(_:)) ||
+            action == #selector(UIResponderStandardEditActions.copy(_:)) {
+            return false
+        }
+        return super.canPerformAction(action, withSender: sender)
     }
 }
