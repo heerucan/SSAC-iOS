54996df4565b17d3c8cd3ff347a84d310fdb116a
heerucan <iwpow0307@gmail.com>
2022-08-03 21:42:02 +0900

    [#10] ReusableProtocol 생성해서 뷰컨/셀 Identifier 초기 정의 간편화

--- TMDB/TMDB/Global/ReusableViewProtocol.swift
+++ TMDB/TMDB/Global/ReusableViewProtocol.swift
@@ -5,4 +5,20 @@
 //  Created by heerucan on 2022/08/03.
 //
 
-import Foundation
+import UIKit
+
+protocol ReusableViewProtocol {
+    static var identifier: String { get }
+}
+
+extension UIViewController: ReusableViewProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableViewProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
