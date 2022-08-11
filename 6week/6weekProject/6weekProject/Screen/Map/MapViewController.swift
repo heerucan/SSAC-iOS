328e7782c789f9faa7404e309677473fb9a57c6e
heerucan <iwpow0307@gmail.com>
2022-08-11 12:05:15 +0900

    [#11] Info.plist에서 위치 권한 설정 WhenInUse

---
+++ 6week/6weekProject/6weekProject/Screen/Map/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  6weekProject
+//
+//  Created by heerucan on 2022/08/11.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
