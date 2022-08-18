171fc2fc91be5ff7685799f2860874ba67441702
heerucan <iwpow0307@gmail.com>
2022-08-18 12:47:39 +0900

    [#12, #10] Generic을 통해서 화면전환 함수 Extension으로 분리

--- TMDB/TMDB/Global/Extension/UIViewController+.swift
+++ TMDB/TMDB/Global/Extension/UIViewController+.swift
@@ -5,4 +5,12 @@
 //  Created by heerucan on 2022/08/18.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func transitionViewController<T: UIViewController>(storyboard sb: String, viewController vc: T) {
+        let storyboard = UIStoryboard(name: sb, bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: T.reuseIdentifier) as? T else { return }
+        self.present(viewController, animated: true)
+    }
+}
