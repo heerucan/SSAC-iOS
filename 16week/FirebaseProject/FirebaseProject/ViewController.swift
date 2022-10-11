80896e404e78668b53b1199367ce91bf6093e7e0
heerucan <iwpow0307@gmail.com>
2022-10-11 09:22:30 +0900

    [#16] Firebase Project 추가 - Firebase Messaging SPM 추가

---
+++ 16week/FirebaseProject/FirebaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseProject
+//
+//  Created by heerucan on 2022/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
